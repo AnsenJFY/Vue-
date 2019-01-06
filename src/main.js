4f0d1b9606bad76109b97433702fa4bc8940625d
AnsenJ <ansen2014@foxmail.com>
2019-01-07 00:41:57 +0800

    项目初始化，css重制，点击时间优化‘

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import fastclick from 'fastclick'
 import store from './store'
+import './assets/style/reset.css';
+import './assets/style/border.css';
 
 Vue.config.productionTip = false
+fastclick.attach(document.body); // 解决300毫秒点击延迟
 
 new Vue({
   router,
