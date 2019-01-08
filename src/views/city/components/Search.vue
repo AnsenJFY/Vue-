9c081b6612408e1bf3dae68d16ca0e89dc71880a
AnsenJ <ansen2014@foxmail.com>
2019-01-08 18:35:51 +0800

    完成城市搜索的功能，这里对数据的操作是非常值得学习的，日常很多地方都会使用到

--- src/views/city/components/Search.vue
+++ src/views/city/components/Search.vue
@@ -1,28 +1,96 @@
 <template>
-  <div class="search">
-    <input type="text" class="search-input" placeholder="输入城市名或拼音">
+  <div>
+    <div class="search">
+      <input type="text" v-model="keyword" class="search-input" placeholder="输入城市名或拼音">
+    </div>
+    <div class="search-content" v-show="keyword" ref="search">
+      <ul>
+        <li class="search-item border-bottom" v-for="item of list" :key="item.id">{{item.name}}</li>
+        <li class="search-item border-bottom" v-show="hasNoData">没有找到匹配数据</li>
+      </ul>
+    </div>
   </div>
 </template>
 <script>
+import Bscroll from "better-scroll";
 export default {
-  name:'CitySearch'
-}
+  name: "CitySearch",
+  props: {
+    cities: Object
+  },
+  data() {
+    return {
+      keyword: "",
+      list: [],
+      timer: null
+    };
+  },
+  mounted() {
+    this.scroll = new Bscroll(this.$refs["search"]);
+  },
+  computed: {
+    hasNoData() {
+      return !this.list.length;
+    }
+  },
+  watch: {
+    keyword() {
+      if (this.timer) {
+        clearTimeout(this.timer);
+      }
+      if (!this.keyword) {
+        this.list = [];
+        return;
+      }
+      this.timer = setTimeout(() => {
+        const result = [];
+        for (let i in this.cities) {
+          this.cities[i].forEach(val => {
+            if (
+              val.spell.indexOf(this.keyword) > -1 ||
+              val.name.indexOf(this.keyword) > -1
+            ) {
+              result.push(val);
+            }
+          });
+        }
+        this.list = result;
+      }, 100);
+    }
+  }
+};
 </script>
 <style lang="less" scoped>
-  @import "~@style/varibles.less";
-.search{
-  padding: 0 .1rem;
-  height: .72rem;
+@import "~@style/varibles.less";
+.search {
+  padding: 0 0.1rem;
+  height: 0.72rem;
   background: @bg-color;
-  .search-input{
+  .search-input {
     box-sizing: border-box;
     width: 100%;
-    height: .62rem;
-    line-height: .62rem;
+    height: 0.62rem;
+    line-height: 0.62rem;
     text-align: center;
-    border-radius: .06rem;
+    border-radius: 0.06rem;
     color: #666;
-    padding: 0 .1rem;
+    padding: 0 0.1rem;
+  }
+}
+.search-content {
+  overflow: hidden;
+  z-index: 1;
+  position: absolute;
+  top: 1.58rem;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: #eee;
+  .search-item {
+    line-height: 0.62rem;
+    padding-left: 0.2rem;
+    color: #666;
+    background: #fff;
   }
 }
 </style>
